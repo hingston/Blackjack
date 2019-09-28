2b8bccc07a660b2d99c7510da72da5139b724eaa
William Hingston <will.hingston@gmail.com>
2019-09-28 10:22:31 +0100

    Added documentation

--- CardGames.py
+++ CardGames.py
@@ -1,17 +1,42 @@
 from typing import List
 
-from Decks import Deck, Card
+from Decks import FrenchDeck, Card
 
 
 class Blackjack:
+    """Used to play a simple game of Blackjack
+
+    Plays a game of Blackjack using the console. A single player plays against the dealer. The player has the option of
+    hitting or standing. The dealer must hit when on or below 17. The player wins by having a hand value greater than
+    that of the dealer.
+
+    Attributes:
+        dealer_hand: A list of cards in the players hand.
+        player_hand: A list of cards in the dealers hand.
+    """
     dealer_hand = []
     player_hand = []
 
-    def __init__(self, deck: Deck) -> None:
+    def __init__(self, deck: FrenchDeck) -> None:
+        """Inits Blackjack class with a French deck of cards"""
         self.deck = deck
 
     @staticmethod
     def max_hand_value(hand: List[Card]) -> int:
+        """Calculates maximum hand value
+
+        Calculates the maximum value of a Blackjack hand. This is done by first looping through a hand adding the value
+        the card if a number, 10 for kings/queens/jacks and 11 for aces while counting the number of aces. Then looping
+        a final time for the number of aces in the hand and subtracting 10 while the score is higher than 21. This
+        ensures the maximum hand value is found when there are more than 1 ace in the hand without having to sort the
+        before the initial loop.
+
+        Args:
+            hand: A list of cards
+
+        Returns:
+            An integer of the maximum hand value
+        """
         score = 0
         aces = 0
         for card in hand:
@@ -31,13 +56,25 @@ class Blackjack:
 
     @staticmethod
     def hand_to_str(hand: List[Card]) -> str:
+        """Converts a list of cards to a short human readable string.
+
+        For example: [(Diamonds, 6), (Diamonds, 5), (Spades, K)] -> D-6, D-5, S-K
+
+        Args:
+            hand: A list of cards
+
+        Returns:
+            A string with a short human readable hand.
+        """
         return ", ".join(["-".join(card) for card in hand])
 
     def print_current_hands(self) -> None:
+        """Displays the dealer and player's current hand value and cards"""
         print("Dealer: %02d - Hand: %s." % (self.max_hand_value(self.dealer_hand), self.hand_to_str(self.dealer_hand)))
         print("Player: %02d - Hand: %s." % (self.max_hand_value(self.player_hand), self.hand_to_str(self.player_hand)))
 
     def play(self) -> None:
+        """"""
         self.deck.shuffle()
         play_again = True
         play_forever = False
--- Decks.py
+++ Decks.py
@@ -6,31 +6,66 @@ Card = namedtuple("Card", "suit value")
 
 
 class Deck:
+    """Base class for a deck of cards
+
+    Attributes:
+        suits: A list of suits
+        values: A list of values
+    """
     suits = []
     values = []
 
     def __init__(self) -> None:
+        """Inits a deck with cards"""
         self.cards = []
         self.new()
 
     def __str__(self) -> str:
+        """Converts a list of cards to a short human readable string.
+
+        For example: [(Diamonds, 6), (Diamonds, 5), (Spades, K)] -> D-6, D-5, S-K
+        """
         return ", ".join(["-".join(card) for card in self.cards])
 
     def new(self) -> None:
+        """Adds a full new set of cards to the deck based on the suit and values attributes."""
         for card in product(self.suits, self.values):
             self.cards.append(Card(*card))
 
     def shuffle(self) -> None:
+        """Randomly shuffles the deck"""
         shuffle(self.cards)
 
     def draw(self) -> Card:
-        return self.cards.pop()
+        """Draws a card from the end of the deck
+
+        Simulates taking a card from the top of a face down deck.
+
+        Returns:
+            A card
+
+        Raises:
+            IndexError if the deck is empty.
+        """
+        try:
+            return self.cards.pop()
+        except IndexError:
+            raise IndexError("The deck is empty!")
 
     @property
     def empty(self) -> bool:
+        """Property of deck stating if deck is empty or not"""
         return len(self.cards) == 0
 
 
 class FrenchDeck(Deck):
+    """A French deck of cards that inherits from class Deck
+
+    A classic French deck of playing cards. https://en.wikipedia.org/wiki/French_playing_cards
+
+    Attributes:
+        suits: A list of suits. Clubs, Diamonds, Hearts and Spades
+        values: A list of values. Ace, 2-10, Jack, Queen and King
+    """
     suits = ["C", "D", "H", "S"]
     values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
